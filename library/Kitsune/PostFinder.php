27289ea3f923ad35aee183b9ebcac0354836d9df
Nikolaos Dimopoulos <nikos@niden.net>
2015-06-08 22:12:27 -0400

    Created PostsFinder class, used to traverse through the whole array of posts.

---
+++ library/Kitsune/PostFinder.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ +------------------------------------------------------------------------+
+ | Kitsune                                                                |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2013-2015 Phalcon Team and contributors                  |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file docs/LICENSE.txt.                        |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to license@phalconphp.com so we can send you a copy immediately.       |
+ +------------------------------------------------------------------------+
+*/
+
+/**
+ * PostFinder.php
+ * \Kitsune\PostFinder
+ *
+ * Allows faster searching of blog posts
+ */
+namespace Kitsune;
+
+use Kitsune\Exceptions\Exception as KException;
+
+class PostFinder
+{
+    private $data  = [];
+    private $tags  = [];
+    private $links = [];
+    private $dates = [];
+
+    public function __construct()
+    {
+        $sourceFile = K_PATH . '/var/config/posts.json';
+
+        if (!file_exists($sourceFile)) {
+            throw new KException('Posts JSON file cannot be located');
+        }
+
+        $contents = file_get_contents($sourceFile);
+        $data     = json_decode($contents, true);
+
+        if (false === $data) {
+            throw new KException('Posts JSON file is potentially corrupted');
+        }
+
+        /**
+         * First all the data will go in a master array
+         */
+        foreach ($data as $post) {
+            $slug = $post['slug'];
+            $link = $post['link'];
+            $date = $post['date'];
+
+            /**
+             * Add the post in the master array
+             */
+            $this->data[$slug] = $post;
+
+            /**
+             * Tags
+             */
+            $tags = explode(',', $post['tags']);
+            foreach ($tags as $tag) {
+                $this->tags[trim($tag)][] = $slug;
+            }
+
+            /**
+             * Links
+             */
+            $this->links[$link] = $slug;
+
+            /**
+             * Dates (sorting)
+             */
+            $this->dates[$date] = $slug;
+        }
+
+        /**
+         * Sort the dates array
+         */
+        ksort($this->dates);
+    }
+}
