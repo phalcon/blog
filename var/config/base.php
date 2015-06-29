87c57f09ace1cee2ca22cec17d7c68923bcb34b5
Nikolaos Dimopoulos <nikos@niden.net>
2015-06-29 18:12:07 -0400

    Added missing cache setion in the config (base)

--- var/config/base.php
+++ var/config/base.php
@@ -4,6 +4,34 @@ return [
     'debugMode'  => 0,
     'baseUri'    => '/blog/',
     'cdnUrl'     => '',
+    'cache_data' => [
+        'front' => [
+            'adapter' => 'Data',
+            'params'  => [
+                'lifetime' => 86400
+            ]
+        ],
+        'back'  => [
+            'adapter' => 'File',
+            'params'  => [
+                'cacheDir' => K_PATH . '/var/data/cache/'
+            ]
+        ]
+    ],
+    'cache_view' => [
+        'front' => [
+            'adapter' => 'Output',
+            'params'  => [
+                'lifetime' => 86400
+            ]
+        ],
+        'back'  => [
+            'adapter' => 'File',
+            'params'  => [
+                'cacheDir' => K_PATH . '/var/data/cache/'
+            ]
+        ]
+    ],
     'paths'      => [
         'controllersDir' => '',
         'viewsDir'       => '',
