5de2eba90e5b3f8655161da91828a4a4f0600c9e
Nikolaos Dimopoulos <nikos@niden.net>
2015-07-11 11:13:53 -0400

    Corrections for PHPCS PSR2

--- public/index.php
+++ public/index.php
@@ -6,13 +6,11 @@ use \Kitsune\Bootstrap;
 error_reporting(E_ALL);
 
 try {
-
     require_once '../library/Kitsune/Bootstrap.php';
     $di = new PhDI();
     echo Bootstrap::run($di, []);
 
 } catch (\Exception $e) {
-
     $logger = $di->getShared('logger');
     $logger->error($e->getMessage());
     $logger->error('<pre>' . $e->getTraceAsString() . '</pre>');
