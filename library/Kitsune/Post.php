92631258361439a22dac957700183cff068a4375
Nikolaos Dimopoulos <nikos@niden.net>
2015-07-12 21:54:17 -0400

    Changed the class to use getters vs. public properties (read-only class)

--- library/Kitsune/Post.php
+++ library/Kitsune/Post.php
@@ -26,54 +26,110 @@ use \Phalcon\DI\Injectable as PhInjectable;
 
 class Post extends PhInjectable
 {
-    public $title      = '';
-    public $content    = '';
-    public $raw        = '';
-    public $date       = '';
-    public $year       = '';
-    public $month      = '';
-    public $link       = '';
-    public $tags       = [];
-    public $file       = '';
-    public $disqus_id  = '';
-    public $disqus_url = '';
+    private $data = [];
+
+    public function getTitle()
+    {
+        return $this->data['title'];
+    }
+
+    public function getSlug()
+    {
+        return $this->data['slug'];
+    }
+
+    public function getDate()
+    {
+        return $this->data['date'];
+    }
+
+    public function getYear()
+    {
+        return $this->data['year'];
+    }
+
+    public function getMonth()
+    {
+        return $this->data['month'];
+    }
+
+    public function getContent()
+    {
+        return $this->data['content'];
+    }
+
+    public function getRaw()
+    {
+        return $this->data['raw'];
+    }
+
+    public function getLink()
+    {
+        return $this->data['link'];
+    }
+
+    public function getTags()
+    {
+        return $this->data['tags'];
+    }
+
+    public function getFile()
+    {
+        return $this->data['file'];
+    }
+
+    public function getDisqusId()
+    {
+        return $this->data['disqusId'];
+    }
+
+    public function getDisqusUrl()
+    {
+        return $this->data['disqusUrl'];
+    }
 
     public function __construct($post)
     {
-        $dateParts      = explode("-", $post['date']);
-        $this->year     = $dateParts[0];
-        $this->month    = $dateParts[1];
-        $this->slug     = $post['slug'];
-        $this->link     = $post['link'];
-        $this->date     = $post['date'];
-        $this->title    = $post['title'];
+        /**
+         * Initialize the internal array
+         */
+        $this->init();
+
+        $dateParts           = explode("-", $post['date']);
+        $this->data['year']  = $dateParts[0];
+        $this->data['month'] = $dateParts[1];
+        $this->data['date']  = $post['date'];
+        $this->data['slug']  = $post['slug'];
+        $this->data['title'] = $post['title'];
+        $this->data['link']  = $post['link'];
 
         /**
          * Old Tumblr posts have a link so we need the unique identifier
          * from them to be able to link to disqus
          */
-        if ($this->link) {
-            $this->disqus_url = 'http://phalconphp.tumblr.com/post/'
-                              . $this->link;
-            $this->disqus_id = 'Phalcon Framework - ' . $this->title;
+        if ($this->getLink()) {
+            $this->data['disqusUrl'] = 'http://phalconphp.tumblr.com/post/'
+                                     . $this->getLink();
+            $this->data['disqusId']  = 'Phalcon Framework - '
+                                     . $this->getTitle();
         } else {
-            $this->disqus_url = 'https://blog.phalconphp.com/post/'
-                              . $this->slug;
-            $this->disqus_id = 'Phalcon Framework - ' .
-                                str_replace(['"', "''"], ['', ''], $this->title);
+            $this->data['disqusUrl'] = 'https://blog.phalconphp.com/post/'
+                                     . $this->getSlug();
+            $this->data['disqusId']  = 'Phalcon Framework - ' .
+                                     str_replace(['"', "''"], ['', ''], $this->getTitle());
         }
 
-        $this->file      = sprintf(
+        $this->data['file'] = sprintf(
             '%s/%s/%s-%s.md',
             $dateParts[0],
             $dateParts[1],
-            $this->date,
-            $this->slug
+            $this->getDate(),
+            $this->getSlug()
         );
 
         $tags = explode(',', $post['tags']);
         foreach ($tags as $tag) {
-            $this->tags[] = trim($tag);
+            $this->data['tags'][] = trim($tag);
         }
 
         /**
@@ -84,11 +140,29 @@ class Post extends PhInjectable
         /**
          * Get the post itself
          */
-        $fileName = K_PATH . '/data/posts/' . $this->file;
+        $fileName = K_PATH . '/data/posts/' . $this->getFile();
         if (file_exists($fileName)) {
-            $this->raw     = file_get_contents($fileName);
-            $this->raw     = str_replace('{{ cdnUrl }}', $cdnUrl, $this->raw);
-            $this->content = $this->markdown->render($this->raw);
+            $this->data['raw']     = file_get_contents($fileName);
+            $this->data['raw']     = str_replace('{{ cdnUrl }}', $cdnUrl, $this->getRaw());
+            $this->data['content'] = $this->markdown->render($this->getRaw());
         }
+    }
+
+    private function init()
+    {
+        $this->data = [
+            'title'     => '',
+            'slug'      => '',
+            'date'      => '',
+            'year'      => '',
+            'month'     => '',
+            'content'   => '',
+            'raw'       => '',
+            'link'      => '',
+            'tags'      => [],
+            'disqusId'  => '',
+            'disqusUrl' => '',
+            'file'      => '',
+        ];
     }
 }
