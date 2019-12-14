63b1e09e09b670fb27a8656dacb2e78fd9730df8
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-14 18:23:10 -0500

    Trying new format

---
+++ updateData.php
@@ -0,0 +1,24 @@
+<?php
+
+$base      = "/opt/build/repo/";
+echo "Updating Sponsors" . PHP_EOL;
+
+$data = file_get_contents(
+    'https://raw.githubusercontent.com/phalcon/assets/master/phalcon/sponsors-fragment.html'
+);
+
+file_put_contents(
+    $base . '_includes/sponsors.html',
+    $data
+);
+
+echo "Updating Fan Art" . PHP_EOL;
+
+$data = file_get_contents(
+    'https://raw.githubusercontent.com/phalcon/assets/master/phalcon/fanart-fragment.html'
+);
+
+file_put_contents(
+    $base . '_includes/fanart.html',
+    $data
+);
