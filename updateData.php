770f6f36543eaa63146b59ad435d2ca70ff45ef2
Nikolaos Dimopoulos <nikos@niden.net>
2020-02-03 21:58:01 -0500

    Updated footer

--- updateData.php
+++ updateData.php
@@ -22,3 +22,15 @@ file_put_contents(
     $base . '_includes/fanart.html',
     $data
 );
+
+echo "Updating Footer" . PHP_EOL;
+
+$data = file_get_contents(
+    'https://raw.githubusercontent.com/phalcon/assets/master/phalcon/footer-fragment.html'
+);
+
+file_put_contents(
+    $base . '_includes/footer.html',
+    $data
+);
+
